b547d7a5a7c1730af5d14ab8fc5e746f778f6b48
mchorazak <michochozaku@gmail.com>
2019-08-07 09:22:01 +0100

    test package schema with epics alarms

--- conanfile.py
+++ conanfile.py
@@ -5,7 +5,7 @@ from conans.util import files
 
 class StreamingDataTypesConan(ConanFile):
     name = "streaming-data-types"
-    version = "fb4d847"
+    version = "088565c"
     license = "BSD 2-Clause"
     url = "https://bintray.com/ess-dmsc/streaming-data-types"
     settings = "compiler", "arch"
